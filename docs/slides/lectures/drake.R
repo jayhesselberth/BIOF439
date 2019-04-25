42d09ee23ef4306c8c04e7a72e84d32c49e4d1b3
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-04-25 05:11:04 -0400

    really added pdfs

--- docs/slides/lectures/drake.R
+++ docs/slides/lectures/drake.R
@@ -2,6 +2,7 @@ library(drake)
 library(fs)
 suppressPackageStartupMessages(library(tidyverse))
 
+outdir <- here::here('docs','slides','lectures')
 files <- dir_ls(path=here::here('slides/lectures'), glob='*.Rmd', type='file')
 in_pdf <- files %>% map_chr(~str_replace(.,'Rmd','pdf'))
 out_pdf <- files %>% map_chr(~str_replace(., 'slides','docs/slides')) %>%
@@ -10,7 +11,8 @@ out_html <- files %>% map_chr(~str_replace(., 'slides','docs/slides')) %>%
     map_chr(~str_replace(., 'Rmd','html'))
 
 mv_pdf <- function(f){
-  file_move(f, str_replace(f, 'slides','docs/slides'))
+  if(file_exists(f)){
+  file_move(f, str_replace(f, 'slides','docs/slides'))}
 }
 
 plan <- drake_plan(
@@ -21,7 +23,7 @@ plan <- drake_plan(
 )
 plan_pdf <- drake_plan(
  render_pdf = target(
-    pagedown::chrome_print(infile),
+    pagedown::chrome_print(infile, output = fs::path(outdir, str_replace(basename(infile), 'Rmd','pdf'))),
     transform = map(infile = !!files)
   ),
  move_pdf = target(
