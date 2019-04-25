bd6305607007a0ed5f02c604b6e9e59aff2b883f
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-04-25 00:47:11 -0400

    slides

---
+++ slides/lectures/ggplot_exercises/exercise1.R
@@ -0,0 +1,23 @@
+## Exercise 1
+##
+
+## Read the spine data
+
+library(tidyverse) # includes ggplot2
+library(rio)
+dat_spine <- import('data/Dataset_spine.csv', check.names = T)
+
+## Take a look at this data
+
+View(dat_spine)
+
+## TODO
+## Using this data, draw a scatter plot looking at the relationship between
+## the sacral slope and the degree of spondylolisthesis
+
+
+
+
+
+
+
