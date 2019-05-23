926e597ca83d2e931b75e59b0fd69c20e7c05c32
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-05-22 20:07:48 -0400

    updated projects

--- docs/FinalProjects/Jacob_Noeker/install.R
+++
@@ -1,6 +0,0 @@
-install.packages('tidyverse')
-install.packages('devtools')
-install.packages(c('flexdashboard','ggrepel','GGally','cowplot','ggpubr'))
-install.packages('BiocManager')
-BiocManager::install()
-
